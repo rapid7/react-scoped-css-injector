16606ce6a0117ab323e25bce76349dbb148dd3c6
tquetano-r7 <tony_quetano@rapid7.com>
2016-11-11 07:57:47 -0500

    Updates... * Add support for LESS processing * Add support for passing renderOptions * Move various Promise-based methods to their own utility methods (more testable)

---
+++ test/transform/utils.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+
+import {
+  applyCssModules,
+  generateTransformedStylesFile,
+  getPrefixedCss,
+  getRenderedCss,
+  resolveUrlPath,
+  saveFile
+} from 'src/transform/utils';
+
+test.todo('applyCssModules');
+test.todo('generateTransformedStylesFile');
+test.todo('getPrefixedCss');
+test.todo('getRenderedCss');
+test.todo('resolveUrlPath');
+test.todo('saveFile');
