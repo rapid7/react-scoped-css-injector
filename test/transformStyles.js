6110ff7659f3590d496fcb82339905e02620bde5
tquetano-r7 <tony_quetano@rapid7.com>
2016-11-11 16:50:10 -0500

    Fixes... * Remove unsafe cssnano transformations * Update tests

--- test/transformStyles.js
+++ test/transformStyles.js
@@ -8,11 +8,11 @@ import transformStyles from 'src/transformStyles';
 
 test.serial('if transformStyles will convert the styles to an object correctly with scss files', async (t) => {
   const selector = 'foo';
-  const cssString = 'display: block;';
   const hash = 'foobar';
+  const source = 'helpers/foo.scss';
 
   const sassStub = sinon.stub(sass, 'renderSync', () => {
-    const css = new Buffer(`.${selector} {${cssString}}`);
+    const css = fs.readFileSync(source);
 
     return {
       css
@@ -22,8 +22,40 @@ test.serial('if transformStyles will convert the styles to an object correctly w
 
   const result = await transformStyles({
     hash,
-    source: 'bar.scss',
-    target: 'baz.css'
+    source,
+    target: 'bar.css'
+  });
+
+  t.deepEqual(result, {
+    css: `._${hash}__${selector}{display:block}`,
+    id: `${hash}_Scoped_Styles`,
+    selectors: {
+      foo: `_${hash}__${selector}`
+    }
+  });
+
+  sassStub.restore();
+  fsStub.restore();
+});
+
+test.serial('if transformStyles will convert the styles to an object correctly with less files', async (t) => {
+  const selector = 'foo';
+  const hash = 'foobar';
+  const source = 'helpers/foo.less';
+
+  const sassStub = sinon.stub(sass, 'renderSync', () => {
+    const css = fs.readFileSync(source);
+
+    return {
+      css
+    };
+  });
+  const fsStub = sinon.stub(fs, 'writeFileSync');
+
+  const result = await transformStyles({
+    hash,
+    source,
+    target: 'bar.css'
   });
 
   t.deepEqual(result, {
@@ -40,17 +72,13 @@ test.serial('if transformStyles will convert the styles to an object correctly w
 
 test.serial('if transformStyles will convert the styles to an object correctly with css files', async (t) => {
   const selector = 'foo';
-  const cssString = 'display: block;';
   const hash = 'foobar';
 
-  const fsReadFileStub = sinon.stub(fs, 'readFileSync', () => {
-    return new Buffer(`.${selector} {${cssString}}`);
-  });
   const fsWriteFileStub = sinon.stub(fs, 'writeFileSync');
 
   const result = await transformStyles({
     hash,
-    source: 'bar.css',
+    source: 'helpers/foo.css',
     target: 'baz.css'
   });
 
@@ -62,6 +90,5 @@ test.serial('if transformStyles will convert the styles to an object correctly w
     }
   });
 
-  fsReadFileStub.restore();
   fsWriteFileStub.restore();
 });
